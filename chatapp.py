a5b05441468c7885499c99350fad1e4cf9c2a50a
Idan from pi <idan.itzhaky@gmail.com>
2020-01-16 12:56:31 +0000

    pi changes

--- chatapp.py
+++ chatapp.py
@@ -3,7 +3,9 @@ from PIL import Image, ImageDraw, ImageFont
 
 
 from papirus import Papirus
+from papirus import PapirusButtonsHandler
 from views import ButtonsLabelsView
+from controllers import KeyboardInputController
 
 WHITE = 255
 BLACK = 0
@@ -11,8 +13,8 @@ BLACK = 0
 class ChatApp():
 
     def __init__(self):
-        self.papirus = Papirus(rotation=180)
-        self.papirus.clear()
+        #self.papirus = Papirus(rotation=180)
+        #self.papirus.clear()
 
         self.buttons = PapirusButtonsHandler()
         self.buttons.add_listener(self.button_press)
@@ -21,11 +23,14 @@ class ChatApp():
         self.height = 96
 
         # views
-        keyboard_height = 30
-        keyboard_view = KeyboardView(
-            (0, height - keyboard_height),
-            (width, keyboard_height),
+        buttons_labels__view_height = 30
+        self.buttons_labels_view = KeyboardView(
+            (0, height - buttons_labels_view_height),
+            (width, buttons_labels_view_height),
             color=WHITE, bgcolor=BLACK)
+
+        # controllers
+	self.keyboard = KeyboardInputController()
 
         #firstdraw
         self.draw(True)
@@ -35,15 +40,16 @@ class ChatApp():
 
         draw = ImageDraw.Draw(im)
 
-        self.keyboard_view.draw(draw)
+        self.buttons_labels_view.set_labels(keyboard.get_labels)
+	self.buttons_labels_view.draw(draw)
 
         del draw
-
-        self.papirus.display(im)
-        if first_draw:
-            self.papirus.update()
-        else
-            self.papirus.partial_update()
+	
+        #self.papirus.display(im)
+        #if first_draw:
+        #    self.papirus.update()
+        #else:
+        #    self.papirus.partial_update()
 
     def button_press(self, buttonIndex):
         self.keyboard.button_press(buttonIndex)
--- papirus/buttons.py
+++ papirus/buttons.py
@@ -7,41 +7,43 @@ SW3 = 20
 SW4 = 19
 SW5 = 26
 
-import thread
+import threading
 import time
 
 class PapirusButtonsHandler(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
-        self.setup()
-        self.button_pins = (SW5, SW4, SW3, SW2, SW1)
+        self.buttons_pins = [SW5, SW4, SW3, SW2, SW1]
         self.lastValues = [False, False, False, False, False]
         self.listeners = []
-        self.running = false
+        self.running = False
+	self.setup()
 
     def setup(self):
         GPIO.setmode(GPIO.BCM)
-        for pin in self.button_pins:
+        for pin in self.buttons_pins:
             GPIO.setup(pin, GPIO.IN)
 
     def add_listener(self, listener):
-        self.listeners.append(listener);
+        self.listeners.append(listener)
         if not self.running:
             self.run()
 
     def notifyPress(self, buttonIndex):
+	print "button pressed " + str(buttonIndex)
         for listener in self.listeners:
             listener(buttonIndex)
 
     def run(self):
         self.running = True
         while True:
-            values = [GPIO.input(pin) == True for pin in self.button_pins]
+            values = [GPIO.input(pin) == False for pin in self.buttons_pins]
+	    print values
             #numButtonsPressed = sum(1 if buttonValue == True else 0 for buttonValue in values)
 
-            for buttonIndex in range(values):
-                if self.lastValues[buttonIndex] == True
-                        and values[buttonIndex] == False:
-                        self.notifyPress(buttonIndex)
+            for buttonIndex in range(len(values)):
+                if self.lastValues[buttonIndex] == True and values[buttonIndex] == False:
+                	self.notifyPress(buttonIndex)
 
-            time.sleep(0.2)
+            self.lastValues = values
+	    time.sleep(1)
